397d718f68c5e12f243b61254327e8bd5aedf9cc
izunna1026 <done4emma@yahoo.com>
2023-09-08 14:13:50 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
