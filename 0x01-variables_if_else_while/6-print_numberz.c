9fbe148b734208b85fca5d60652c0b90786edce9
izunna1026 <done4emma@yahoo.com>
2023-09-08 15:41:31 -0700

    task6 print numbv

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Main -Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+		putchar(k + '0');
+	putchar('\n');
+	return (0);
+}
