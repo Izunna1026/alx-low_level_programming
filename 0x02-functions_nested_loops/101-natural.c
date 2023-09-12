537e36b35eed123bcee66cf8f1c77c3164a78acc
izunna1026 <done4emma@yahoo.com>
2023-09-12 15:57:56 -0700

    task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the starting point
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
