6b3174f3a24938cd9f2946c405ff30ee5cd3c12a
izunna1026 <done4emma@yahoo.com>
2023-09-14 04:32:38 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - to multiply a function
+ * @a: the first variable
+ * @b: the second variable
+ * Return: to return to the main file
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
