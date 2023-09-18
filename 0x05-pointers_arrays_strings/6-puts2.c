1b58a4c5dd4646b43d8bf25f58dafb163c63a86c
izunna1026 <done4emma@yahoo.com>
2023-09-18 09:16:37 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,11 @@
 void puts2(char *str)
 {
 	int i;
-	int j = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-		j++;
-	for (i = 0; i <= j; i += 2)
-		_putchar(str[i]);
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
