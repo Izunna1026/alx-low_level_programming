9502a4aaab47febb10cf37d227aab13ee59baf1e
izunna1026 <done4emma@yahoo.com>
2023-09-21 13:21:11 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *string_toupper - function to convert to upper
+ *@str: the variable
+ *Return: to return the variable str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
