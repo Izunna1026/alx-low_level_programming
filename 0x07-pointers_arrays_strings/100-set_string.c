3656290bd2b4c91bddba70870e5fa14c15a24224
izunna1026 <done4emma@yahoo.com>
2023-09-25 13:31:20 -0700

    task100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - the function to set string
+ * @s: the first variable
+ * @to: the input variable
+ * Return: to return 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
