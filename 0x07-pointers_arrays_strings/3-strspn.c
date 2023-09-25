ee441933e79a85bd67617a129bdb27d1f1db2070
izunna1026 <done4emma@yahoo.com>
2023-09-25 12:14:57 -0700

    task4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - function th determin the length
+ * @s: the variabe s
+ * @accept: the variable to accept
+ * Return: to return a value
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
