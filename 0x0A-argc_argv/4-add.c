f35f2826998a2fd24b267627c341af25848115d8
izunna1026 <done4emma@yahoo.com>
2023-10-02 12:52:03 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - to add some values
+ * @argc: the argument counter
+ * @argv: the argument vector
+ * Return: to return 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	unsigned int b, sum = 0;
+	char *c;
+
+	if (argc > 1)
+	{
+		for (a = 1; a < argc; a++)
+		{
+			c = argv[a];
+			for (b = 0; b < strlen(c); b++)
+			{
+				if (c[b] < 48 || c[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(c);
+			c++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
