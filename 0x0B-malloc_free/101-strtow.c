0069be9228c14b2a6c2694a620ae2e374957187c
izunna1026 <done4emma@yahoo.com>
2023-10-03 21:57:11 -0700

    failed

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * word_len - locates the index
+ * @str: the string to be searched
+ * Return: the index marking
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
+/**
+ * count_word - help fnction to count
+ * @s: string to evaluate
+ * Return: to return no of words
+ */
+int count_word(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+	return (words);
+}
+/**
+ * strtow - splits a string
+ * @str: string to split
+ * Return: Pointer to an array of string
+ */
+char **strtow(char *str)
+{
+	char **matrix;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+				matrix[w] = malloc(sizeof(char) * (letters + 1));
+
+				if (matrix[w] == NULL)
+				{
+					for (; w >= 0; w--)
+						free(matrix[w]);
+
+					free(matrix);
+					return (NULL);
+				}
+				for (l = 0; l < letters; l++)
+					matrix[w][l] = str[index++];
+				matrix[w][l] = '\0';
+	}
+	matrix[w] = NULL;
+	return (matrix);
+}
