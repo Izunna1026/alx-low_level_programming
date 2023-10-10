0897c22b2b1d4d1ee469e1b5009b246317394986
Izunna1026 <done4emma@yahoo.com>
2023-10-10 21:38:05 +0000

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog attribute
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
