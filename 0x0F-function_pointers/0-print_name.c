4ec3209aa0a77adf63f7d745cfef92f1f05860f8
Izunna1026 <done4emma@yahoo.com>
2023-10-11 20:28:01 +0000

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function name
+ * @name: variable to print
+ * @f: the variable f
+ * Return: to return 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);
+	return;
+}
