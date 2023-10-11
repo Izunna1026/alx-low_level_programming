6a9e22cc9b7e8fcd67099df5e40cf05e151c0a50
Izunna1026 <done4emma@yahoo.com>
2023-10-11 21:14:47 +0000

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - the function index
+ * @array: the array of figures
+ * @size: size of the variable
+ * @cmp: the variable
+ * Return: to return 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]) == 1)
+					return (i);
+			}
+			
+		}
+	}
+	return (-1);
+}
