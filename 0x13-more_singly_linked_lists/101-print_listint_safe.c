c22791e051b2986cfbaa04f2bd0786f1006124d5
izunna1026 <done4emma@yahoo.com>
2023-10-24 08:48:45 -0700

    task12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,60 @@
+#include "lists.h"
+/**
+ * listed - to list
+ * @head: the first node
+ * Return: to return 0
+ */
+void listed(listint_t **head)
+{
+	listint_t *new, newnode;
+
+	if (head != NULL)
+	{
+		newnode = *head;
+		while ((new = newnode) != NULL)
+		{
+			newnode = newnode->next;
+			free(new);
+		}
+		*head = NULL;
+	}
+}
+/**
+ * print_listint_safe - print the list
+ * @head: the head of the list
+ * Return: to return head
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t elem;
+	listint_t *name, *newnode, *h, *k;
+
+	h = NULL;
+
+	while (head != NULL)
+	{
+		newnode = malloc(sizeof(listint_t));
+		if (newnode == NULL)
+			exit(98);
+		newnode->t = (void *)head;
+		newnode->next = h;
+		h = newnode;
+		k = h;
+
+		while (k->next != NULL)
+		{
+			k = k->next;
+			if (head == k->t)
+			{
+				printf("->[%p] %d\n", (void *)head, head->n);
+				listed(&h);
+				return (elem);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		elem++;
+	}
+	listed(&h);
+	return (elem);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,13 +9,14 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
+ * @p: void
  * Description: singly linked list node structure
  *
  */
 typedef struct listint_s
 {
 	int n;
+	void *t;
 	struct listint_s *next;
 } listint_t;
 
