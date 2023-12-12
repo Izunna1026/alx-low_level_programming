3cbbab370114aecf668e0fdb6c3c22fb476e1124
izunna1026 <done4emma@yahoo.com>
2023-12-11 20:19:36 -0800

    task100

---
+++ 0x18-dynamic_libraries/mat.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+ * add - to sum numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: to return sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - to minus numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: to return sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - to get mod numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: to return mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * mul - to multiply numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: to return multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - to divide numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: to return division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
